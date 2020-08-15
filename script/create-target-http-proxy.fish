36506619eb7c0828c7098bdc491d53981c8c74bc
shinyay <shinya.com@gmail.com>
2020-08-15 21:50:43 +0900

    Add Target Http Proxy

---
+++ script/create-target-http-proxy.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+
+function do_func
+  argparse -n do_func 'h/help' 'n/name=' -- $argv
+  or return 1
+
+  if set -lq _flag_help
+    echo "create-target-http-proxy.fish -n/--name <APP_NAME>"
+    return
+  end
+
+  set -lq _flag_name
+  or set -l _flag_name hello-app
+
+  gcloud compute target-http-proxies create $_flag_name-target-http-proxy --url-map $_flag_name-url-map
+
+  gcloud compute target-http-proxies
+end
+
+do_func $argv
