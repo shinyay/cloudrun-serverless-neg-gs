cef74d495a64161dd2b3373a00670230740a8e5c
shinyay <shinya.com@gmail.com>
2020-08-15 21:37:24 +0900

    Add Create URL Map to Backend Service

---
+++ script/create-url-map.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+
+function do_func
+  argparse -n do_func 'h/help' 'n/name=' -- $argv
+  or return 1
+
+  if set -lq _flag_help
+    echo "create-url-map.fish -n/--name <APP_NAME>"
+    return
+  end
+
+  set -lq _flag_name
+  or set -l _flag_name hello-app
+
+  gcloud compute url-maps create $_flag_name-url-map --default-service $_flag_name-backend-service
+
+  gcloud compute url-maps list
+end
+
+do_func $argv
